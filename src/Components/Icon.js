65b0ae07da52a0f9e13b0786d56985fcd0b62e14
Sahil <ss6611786@gmail.com>
2023-04-19 22:41:16 +0530

    first commit:=i)i have installed react-icons lib.. and i have used here three icons cross circle pen

---
+++ src/Components/Icon.js
@@ -0,0 +1,10 @@
+import { FaTimes,FaRegCircle,FaPen } from "react-icons/fa";
+
+const Icon=({user_icon})=>{
+   switch(user_icon){
+    case "circle":return <FaRegCircle className="icon"/>
+    case "cross":return <FaTimes className="icon"/>
+    default:return <FaPen className="icon"/>
+   }
+}
+export default Icon;
